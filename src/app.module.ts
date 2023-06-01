3b3fe4449db7a4afbee7afe38735e258366cde84
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2023-06-01 15:05:33 +0200

    setting up typeorm to connect to planetscale db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config'; //loads the env variables which are used in the TypeOrmModule config
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    ConfigModule.forRoot(),
+    UsersModule,
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [join(__dirname, '**', '*.entity.{ts,js}')],
+      synchronize: true,
+      ssl: {
+        rejectUnauthorized: true,
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
