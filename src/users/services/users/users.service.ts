918d31daf434c685149c57b0980a4ccd9e2ac1f7
Kenny Tran <82717216+nytrek@users.noreply.github.com>
2023-06-02 08:22:18 +0200

    remove createdAt property from updateUser method

--- src/users/services/users/users.service.ts
+++ src/users/services/users/users.service.ts
@@ -43,7 +43,7 @@ export class UsersService {
 
     return await this.userRepository.update(
       { id: userId },
-      { ...createUserDto, createdAt: new Date(), updatedAt: new Date() },
+      { ...createUserDto, updatedAt: new Date() },
     );
   }
 
